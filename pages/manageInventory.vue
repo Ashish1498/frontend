850678a9ad375f0a07b854ed78758f036c648bda
Meghna Bajoria <meghnabajoria@Meghnas-MacBook-Pro.local>
2021-03-03 20:28:08 +0530

    api of view meds and form done

--- pages/manageInventory.vue
+++ pages/manageInventory.vue
@@ -3,23 +3,42 @@
     <div>
       <navbar />
     </div>
-    <div class= 'btn-inventory'>
-        <button>Add an item</button>
-        <button>Update Inventory</button>
-        <button>Delete an Item</button>
+    <div class="btn-inventory">
+      <button class="big-button" v-on:click="openForm()" @click="toggleAddItemBtn">Add an item</button>
+      <add-med v-show="visible"/>
+      <button class="big-button">Update Inventory</button>
+      <button class="big-button">Delete an Item</button>
     </div>
   </div>
 </template>
 
 <script>
 import Navbar from '@/components/NavBar.vue'
+import AddMed from '@/components/AddMed.vue'
 export default {
-  components: { Navbar }
+  components: { Navbar, AddMed },
+  data () {
+    return {
+      visible: false
+    }
+  },
+  methods: {
+    openForm () {
+      document.getElementById('myForm').style.display = 'block'
+    },
+    closeForm () {
+      document.getElementById('myForm').style.display = 'none'
+    },
+    toggleAddItemBtn () {
+      if (this.visible === true) this.visible = false
+      else this.visible = true
+    }
+  }
 }
 </script>
 
-<style>
-.btn-inventory button{
+<style scoped>
+.big-button {
   padding: 15px 25px;
   font-size: 24px;
   width: 100%;
@@ -37,9 +56,21 @@ export default {
   justify-content: center;
   align-items: center;
 }
+.btn_login button{
+    font-size: 10px !important;
+    width: 50% !important;
+}
 
-.btn-inventory button:hover {background-color: #3e8e41}
-.btn-inventory button:active {
+.btn_close button{
+    font-size: 10px !important;
+    width: 50% !important;
+
+}
+
+.big-button button:hover {
+  background-color: #3e8e41;
+}
+.big-button button:active {
   background-color: #3e8e41;
   box-shadow: 0 5px #666;
   transform: translateY(4px);
--- store/index.js
+++ store/index.js
@@ -5,16 +5,37 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    medicines: []
+    medicines: [],
+    medicineObj: {
+      categoryId: '',
+      costPerUnit: '',
+      dosage: '',
+      expiryDate: '',
+      formula: '',
+      isInStock: '',
+      manufacturer: '',
+      medicineId: '',
+      medicineName: '',
+      mfdDate: '',
+      quantity: '',
+      totalQuantity: '',
+      unit: ''
+    }
   },
   getters: {
     getMedicine (state) {
       return state.medicines
+    },
+    getMedicineObj (state) {
+      return state.medicineObj
     }
   },
   mutations: {
     setMedicine (state, value) {
       state.medicines = value
+    },
+    setMedicineObj (state, value) {
+      state.medicineObj = value
     }
   },
   actions: {
@@ -30,6 +51,31 @@ export default new Vuex.Store({
           commit('setMedicine', e.data)
         })
         .catch(e => console.log('Nooooo'))
+    },
+    setMedicineObjAction ({ commit }, value) {
+      commit('setMedicineObj', value)
+      const axiosConfig = {
+        baseURL: 'http://10.177.68.40:8080/',
+        url: '/medicine/createMedicine',
+        method: 'post',
+        data: {
+          categoryId: value.categoryId,
+          medicineId: value.medicineId,
+          medicineName: value.medicineName,
+          costPerUnit: value.costPerUnit,
+          dosage: value.dosage,
+          mfdDate: value.mfdDate,
+          expiryDate: value.expiryDate,
+          formula: value.formula,
+          isInStock: value.isInStock,
+          manufacturer: value.manufacturer,
+          quantity: value.quantity,
+          totalQuantity: value.totalQuantity
+        }
+      }
+      axios(axiosConfig)
+        .then(e => console.log(e))
+        .catch(e => console.log(e))
     }
   }
 })
